26dcb8f3bdfcc169e77cd8e4abffcc3763b6669f
Zxilly <zxilly@outlook.com>
2022-08-09 18:38:21 +0800

    style: lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC, useEffect, useState } from "react";
+import { ChangeEvent, FC, useEffect, useState } from 'react'
 import {
   Button,
   Container,
@@ -11,95 +11,107 @@ import {
   Grid,
   styled,
   TextField
-} from "@mui/material";
-import { endpoint } from "@/main";
-import { useKey, useLocalStorage } from "react-use";
-import { Problem } from "./Problem";
+} from '@mui/material'
+import { endpoint } from '@/main'
+import { useKey, useLocalStorage } from 'react-use'
+import { Problem } from './Problem'
 
-
-export type Problem = {
-  success: number;
-  fail: number;
-  name: string;
-  onenote_url: string;
+export type ProblemType = {
+  success: number
+  fail: number
+  name: string
+  onenote_url: string
 }
 type ProblemResponse = {
-  [key: string]: Problem;
+  [key: string]: ProblemType
 }
 
 const App: FC = () => {
-  const [problem, setProblem] = useState<ProblemResponse>({});
-  const [section, setSection] = useLocalStorage("section", "");
+  const [problem, setProblem] = useState<ProblemResponse>({})
+  const [section, setSection] = useLocalStorage('section', '')
 
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
 
-  useKey((ev) => {
-    if (ev.ctrlKey && ev.key.toLowerCase() === "k") {
-      ev.preventDefault();
-      return true;
+  useKey(
+    ev => {
+      if (ev.ctrlKey && ev.key.toLowerCase() === 'k') {
+        ev.preventDefault()
+        return true
+      }
+      return false
+    },
+    () => {
+      setOpen(true)
     }
-    return false;
-  }, () => {
-    setOpen(true);
-  });
+  )
 
   useEffect(() => {
     if (section && !open) {
-      fetch(`${endpoint}/stats/_/${section}.json`).then(res => res.json()).then(setProblem);
+      fetch(`${endpoint}/stats/_/${section}.json`)
+        .then(res => res.json())
+        .then(setProblem)
     }
-  }, [section, open]);
-
+  }, [section, open])
 
   function handleDialogClose() {
-    setOpen(false);
+    setOpen(false)
   }
 
   function dialogOnChange(ev: ChangeEvent<HTMLInputElement>) {
-    setSection(ev.target.value);
+    setSection(ev.target.value)
   }
 
   return (
     <Container>
       <Root>
         <CssBaseline />
-        <Dialog open={open} onClose={handleDialogClose} maxWidth={"md"} style={{
-          minWidth: "500px"
-        }}>
+        <Dialog
+          open={open}
+          onClose={handleDialogClose}
+          maxWidth={'md'}
+          style={{
+            minWidth: '500px'
+          }}
+        >
           <DialogTitle>Section Prefix</DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              Set Section ID
-            </DialogContentText>
+            <DialogContentText>Set Section ID</DialogContentText>
             <TextField
               autoFocus
-              margin="dense"
-              label="Section id"
-              type="text"
+              margin='dense'
+              label='Section id'
+              type='text'
               onChange={dialogOnChange}
-              variant="standard"
+              variant='standard'
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={() => {
-              setOpen(false);
-            }}>Cancel</Button>
+            <Button
+              onClick={() => {
+                setOpen(false)
+              }}
+            >
+              Cancel
+            </Button>
             <Button onClick={handleDialogClose}>Save</Button>
           </DialogActions>
         </Dialog>
         <Grid container spacing={2}>
-          {Object.entries(problem).sort().map(([pkey, p]) => {
-            const { success, fail, name, onenote_url } = p;
-            return (
-              <Problem key={pkey} pkey={pkey} success={success} fail={fail} name={name} onenote_url={onenote_url} />
-            );
-          })}
+          {Object.entries(problem)
+            .sort()
+            .map(([pkey, p]) => {
+              const { success, fail, name, onenote_url } = p
+              return (
+                <Problem key={pkey} pkey={pkey} success={success} fail={fail} name={name} onenote_url={onenote_url} />
+              )
+            })}
         </Grid>
       </Root>
     </Container>
-  );
-};
+  )
+}
 
-const Root = styled("div")`
+const Root = styled('div')`
   width: 100%;
   min-height: 95vh;
   display: flex;
@@ -110,6 +122,6 @@ const Root = styled("div")`
     text-decoration: none;
     color: ${({ theme: { palette } }) => palette.primary.main};
   }
-`;
+`
 
-export default App;
+export default App
--- src/Problem.tsx
+++ src/Problem.tsx
@@ -1,129 +1,130 @@
-// noinspection JSIgnoredPromiseFromCall
-
-import { Fragment, MouseEventHandler, useEffect, useState } from "react";
-import { Button, ButtonGroup, Grid, Menu, MenuItem } from "@mui/material";
-import { endpoint } from "@/main";
-import { Problem as ProblemType } from "@/App";
-
+import { Fragment, MouseEventHandler, useEffect, useState } from 'react'
+import { Button, ButtonGroup, Grid, Menu, MenuItem } from '@mui/material'
+import { endpoint } from '@/main'
+import { ProblemType } from '@/App'
 
 interface ProblemProps {
-  pkey: string,
-  success: number,
-  fail: number,
-  name: string,
-  onenote_url: string,
+  pkey: string
+  success: number
+  fail: number
+  name: string
+  onenote_url: string
 }
 
-type colors = "primary" | "success" | "error" | "warning"
+type colors = 'primary' | 'success' | 'error' | 'warning'
 
 export function Problem(props: ProblemProps): JSX.Element {
-  const { pkey, name, onenote_url } = props;
+  const { pkey, name, onenote_url } = props
 
-  const [success, setSuccess] = useState(props.success);
-  const [fail, setFail] = useState(props.fail);
+  const [success, setSuccess] = useState(props.success)
+  const [fail, setFail] = useState(props.fail)
 
-  const [color, setColor] = useState<colors>("primary");
-
-  function updateColor() {
-    if (success > 0) {
-      setColor("success");
-    } else if (fail > 0) {
-      setColor("error");
-    } else {
-      setColor("primary");
-    }
-  }
+  const [color, setColor] = useState<colors>('primary')
 
   useEffect(() => {
-    updateColor();
-  }, [success, fail]);
+    if (success > 0) {
+      setColor('success')
+    } else if (fail > 0) {
+      setColor('error')
+    } else {
+      setColor('primary')
+    }
+  }, [success, fail])
 
   const [contextMenu, setContextMenu] = useState<{
-    mouseX: number;
-    mouseY: number;
-  } | null>(null);
+    mouseX: number
+    mouseY: number
+  } | null>(null)
 
-  const handleContextMenu: MouseEventHandler = (event) => {
-    event.preventDefault();
+  const handleContextMenu: MouseEventHandler = event => {
+    event.preventDefault()
     setContextMenu(
       contextMenu === null
         ? {
-          mouseX: event.clientX + 2,
-          mouseY: event.clientY - 6
-        }
-        :
-        null
-    );
-  };
+            mouseX: event.clientX + 2,
+            mouseY: event.clientY - 6
+          }
+        : null
+    )
+  }
 
   const handleContextMenuClose = () => {
-    setContextMenu(null);
-  };
+    setContextMenu(null)
+  }
 
   function handleSuccessClick() {
     fetch(`${endpoint}/success/${pkey}`, {
-      method: "GET"
-    }).then((res) => res.json()).then((data: ProblemType) => {
-      setSuccess(data.success);
-      setFail(data.fail);
-    });
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then((data: ProblemType) => {
+        setSuccess(data.success)
+        setFail(data.fail)
+      })
   }
 
   function handleFailClick() {
     fetch(`${endpoint}/fail/${pkey}`, {
-      method: "GET"
-    }).then((res) => res.json()).then((data: ProblemType) => {
-      setSuccess(data.success);
-      setFail(data.fail);
-    });
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then((data: ProblemType) => {
+        setSuccess(data.success)
+        setFail(data.fail)
+      })
   }
 
   function handleReset() {
-    setContextMenu(null);
+    setContextMenu(null)
     fetch(`${endpoint}/reset/${pkey}`, {
-      method: "GET"
-    }).then((res) => res.json()).then((data: ProblemType) => {
-      setSuccess(data.success);
-      setFail(data.fail);
-    });
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then((data: ProblemType) => {
+        setSuccess(data.success)
+        setFail(data.fail)
+      })
   }
 
   return (
     <Fragment>
-      <Grid item xs={4} md={3} onContextMenu={handleContextMenu} style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        cursor: "context-menu"
-      }}>
-        <ButtonGroup variant="contained">
+      <Grid
+        item
+        xs={4}
+        md={3}
+        onContextMenu={handleContextMenu}
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          cursor: 'context-menu'
+        }}
+      >
+        <ButtonGroup variant='contained'>
           <Button
             color={color}
             onClick={() => {
-              window.open(onenote_url, "_blank");
-            }}>{name}</Button>
-          <Button
-            variant={"outlined"}
-            color={color}
-            onClick={handleSuccessClick}>{success}</Button>
-          <Button
-            variant={"outlined"}
-            color={color}
-            onClick={handleFailClick}>{fail}</Button>
+              window.open(onenote_url, '_blank')
+            }}
+          >
+            {name}
+          </Button>
+          <Button variant={'outlined'} color={color} onClick={handleSuccessClick}>
+            {success}
+          </Button>
+          <Button variant={'outlined'} color={color} onClick={handleFailClick}>
+            {fail}
+          </Button>
         </ButtonGroup>
       </Grid>
       <Menu
         open={contextMenu !== null}
         onClose={handleContextMenuClose}
-        anchorReference="anchorPosition"
-        anchorPosition={
-          contextMenu !== null
-            ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
-            : undefined
-        }
+        anchorReference='anchorPosition'
+        anchorPosition={contextMenu !== null ? { top: contextMenu.mouseY, left: contextMenu.mouseX } : undefined}
       >
         <MenuItem onClick={handleReset}>Reset</MenuItem>
       </Menu>
     </Fragment>
-  );
+  )
 }
